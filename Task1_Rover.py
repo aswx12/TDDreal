8652d703a1d926f636723c34a8f51dc926c557a2
aswx12 <aswx12@gmail.com>
2026-02-13 15:39:13 +0000

    Update

--- Task1_Rover.py
+++ Task1_Rover.py
@@ -16,7 +16,7 @@ class rovar:
         #if rov is None:
             #return None 
         #This is returning none instead of raising an exception¨
-        #-->  test test s
+        #-->  test test sfsfs
         if normal is None or not isinstance(normal, str):
           raise TypeError("Input must be a non-empty string")
         
