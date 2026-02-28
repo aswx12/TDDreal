e3ddc52dc3940277f6657c857f8a92be948078a7
aswx12 <aswx12@gmail.com>
2026-02-28 12:25:26 +0000

    Update

--- test_Task1_Rover.py
+++ test_Task1_Rover.py
@@ -42,22 +42,22 @@ class test_string(unittest.TestCase):
     # You can continue writing your test cases here based on the assignment description 
 
     #--------------------------Null input---------------------------------
-    def test_enrove_null_input(self): #null input raises bug
+    #def test_enrove_null_input(self): #null input raises bug
       # None should raise TypeError 
-      with self.assertRaises(TypeError):
-        self.rv.enrove(None)
+      #with self.assertRaises(TypeError):
+        #self.rv.enrove(None)
     
-    def test_derove_null_input(self):
-      with self.assertRaises(TypeError):
-          self.rv.derove(None)
+    #def test_derove_null_input(self):
+      #with self.assertRaises(TypeError):
+          #self.rv.derove(None)
 
     #--------------------------Empty string---------------------------------
-    def test_enrove_empty_string(self):
+    #def test_enrove_empty_string(self):
       # if input "" --> should return ""
-      self.assertEqual(self.rv.enrove(""), "")
+      #self.assertEqual(self.rv.enrove(""), "")
     
-    def test_derove_empty_string(self):
-      self.assertEqual(self.rv.derove(""), "")
+    #def test_derove_empty_string(self):
+      #self.assertEqual(self.rv.derove(""), "")
 
     #--------------------------Non-empty Strings---------------------------------
     #consonants sets triggered bugs as both sets missed g respective d
